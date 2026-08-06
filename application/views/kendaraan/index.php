8cad67e8bde83e9626251dc5bf01073599c527fd
!@#Runtuu123 <hendrikocarlos14@gmail.com>
2026-08-06 09:29:14 +0800

    dasd

--- application/views/kendaraan/index.php
+++ application/views/kendaraan/index.php
@@ -278,7 +278,7 @@
                 [9, 'desc']
             ],
             pageLength: 25,
-            lengthMenu: [10, 25, 50, 100],
+            lengthMenu: [10, 25, 50, 100, 200],
             language: {
                 url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/id.json'
             }
